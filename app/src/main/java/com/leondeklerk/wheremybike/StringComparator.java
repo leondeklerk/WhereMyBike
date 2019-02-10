1845cbd93577fa8f8e915bce322d894b0d487ac0
Leon de Klerk <leontheclerk@outlook.com>
2019-02-10 19:44:52 +0100

    Updated sorting for the previous location history

--- app/src/main/java/com/leondeklerk/wheremybike/StringComparator.java
+++ app/src/main/java/com/leondeklerk/wheremybike/StringComparator.java
@@ -1,14 +1,21 @@
 package com.leondeklerk.wheremybike;
 
-import android.util.Log;
-
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Date;
 
 public class StringComparator implements Comparator<String> {
     @Override
-    public int compare(String o1, String o2) {
-        o1 = o1.substring(o1.length() - 16);
-        o2 = o2.substring(o2.length() - 16);
-        return -o1.compareTo(o2);
+    public int compare(String o1, String o2){
+        int result = 0;
+        try {
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date date = df.parse(o1.substring(o1.length() - 18));
+            Date date2 = df.parse(o2.substring(o2.length() - 18));
+            result = date.compareTo(date2);
+        } catch (Exception e){
+            System.err.println(e.toString());
+        }
+        return -result;
     }
 }
